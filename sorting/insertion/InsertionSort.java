dc9d736be63c394e9edc09614e8ac3b6b016f37a
agraca <anthonygraca@gmail.com>
2017-03-23 19:44:43 -0700

    Started Insertion Sort

---
+++ sorting/insertion/InsertionSort.java
@@ -0,0 +1,21 @@
+package sorting.insertion;
+
+import java.util.ArrayList;
+
+public class InsertionSort
+{
+  /**
+  * This algorithm sorts an arraylist of integers into increasing order
+  *
+  * @param a A sequence of n numbers <a1, a2, ..., an>
+  * @return ArrayList<Integer> A sequence of n numbers <a1', a2', ..., an'> such
+  *                            that a1' <= a2' <= ... <= an'
+  **/
+
+  public static void sort(ArrayList<Integer> a)
+  {
+    for (int j = 1; j < a.size(); j++)
+    {
+    }
+  }
+}
