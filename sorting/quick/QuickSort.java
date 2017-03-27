ac9b65141985e0d204a0b2eebe872f5bd6166880
agraca <anthonygraca@gmail.com>
2017-03-27 16:37:49 -0700

    added skeleton for quicksort

---
+++ sorting/quick/QuickSort.java
@@ -0,0 +1,20 @@
+package sorting.quick;
+
+import java.util.ArrayList;
+public class QuickSort implements Command
+{
+  public ArrayList<Integer> quicksort(ArrayList<Integer> a)
+  {
+    return a;
+  }
+
+  public ArrayList<Integer> execute(ArrayList<Integer> a)
+  {
+    return a;
+  }
+
+  public String toString()
+  {
+    return "Quick Sort";
+  }
+}
