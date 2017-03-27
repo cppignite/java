87ae1b6fff46d4322852c320dc8b5a1ccba47e24
agraca <anthonygraca@gmail.com>
2017-03-27 16:42:12 -0700

    updated comment to command.java

--- sorting/Command.java
+++ sorting/Command.java
@@ -1,3 +1,7 @@
+/**
+ * The command pattern is used because I wanted to create a general driver and 
+ * decouple my driver from any specific sorting algorithm
+ **/
 package sorting;
 
 import java.util.ArrayList;
