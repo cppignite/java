2ba869be60a4515669c1383093305b0d30f1ffd7
agraca <anthonygraca@gmail.com>
2017-04-10 15:09:13 -0700

    added method header and comments for recursive matrix multiplication

--- divideAndConquer/Matrix/StrassenMatrixMult.java
+++ divideAndConquer/Matrix/StrassenMatrixMult.java
@@ -2,7 +2,34 @@ import java.util.ArrayList;
 public class StrassenMatrixMult
 {
   /**
+  * This method takes in two square nxn matrices and multiplies them together
+  * using Strassen's divide and conquer algorithm. It runs in O(n^2.81)
+  *
+  * We assume that n is an exact power of 2 so that when we divide an nxn
+  * matrix into four n/2 x n/2 matrices, we are guarenteed that n/2 is an
+  * integer
+  *
+  * Idea:
+  * The idea is that we partition A, B, and C into four n/2 x n/2 matrices
+  * 
+  * @param A - nxn matrix filled with numbers
+  * @param B - nxn matrix filled with numbers
+  * @return C - nxn matrix that's the product of two input matrices
+  **/
+  public ArrayList<ArrayList<Integer> squareMatrixMultipleRecursive(
+      ArrayList<ArrayList<Integer>> a, ArrayList<ArrayList<Integer>> b)
+  {
+     // obtain the dimensions of the square matrix
+    int n = a.size(); 
+    ArrayList<ArrayList<Integer>> c = new ArrayList<ArrayList<Integer>>();
+
+    return c;
+  }
+
+  /**
    * This method takes in two square nxn matrices and multiplies them together
+   * with the definition method. This procedure runes in O(n^3) because each
+   * loop runs n times.
    *
    * @param A - nxn matrix filled with numbers
    * @param B - nxn matrix filled with numbers
@@ -24,7 +51,7 @@ public class StrassenMatrixMult
         for(int k = 0; k < n; k++)
         {
           (c.get(i)).set(j, 
-              (c.get(i)).get(j)+(a.get(i)).get(k)*(b.get(k).get(j));
+              (c.get(i)).get(j)+(a.get(i)).get(k)*(b.get(k).get(j)));
         }
       }
     }
