db9ac4b9ecfe1686ef3350eefa3f1b6ee988403f
agraca <anthonygraca@gmail.com>
2017-04-10 14:47:30 -0700

    added print method

--- divideAndConquer/Matrix/StrassenMatrixMult.java
+++ divideAndConquer/Matrix/StrassenMatrixMult.java
@@ -3,7 +3,6 @@ public class StrassenMatrixMult
 {
   /**
    * This method takes in two square nxn matrices and multiplies them together
-   * via divide and conquer.
    *
    * @param A - nxn matrix filled with numbers
    * @param B - nxn matrix filled with numbers
@@ -15,6 +14,38 @@ public class StrassenMatrixMult
     // obtain the dimensions of the square matrix
     int n = a.size(); 
     ArrayList<ArrayList<Integer>> c = new ArrayList<ArrayList<Integer>>();
+
+    for(int i = 0; i < n; i++)
+    {
+      c.add(new ArrayList<Integer>());
+      for(int j = 0; j < n; j++)
+      {
+        (c.get(i)).add(0);
+        for(int k = 0; k < n; k++)
+        {
+          (c.get(i)).set(j, 
+              (c.get(i)).get(j)+(a.get(i)).get(k)*(b.get(k).get(j));
+        }
+      }
+    }
+
     return c;
+  }
+
+  /**
+   * Prints the matrix
+   *
+   * @param a - the input nxn matrix
+   **/
+  public void printMatrix(ArrayList<ArrayList<Integer>> a)
+  {
+    for(int i = 0; i < a.size(); i++)
+    {
+      for(int j = 0; j < a.size(); j++)
+      {
+        System.out.print((a.get(i)).get(j) + " " );
+      }
+      System.out.println();
+    }
   }
 }
