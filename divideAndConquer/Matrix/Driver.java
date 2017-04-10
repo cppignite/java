22ea58da850c32d891cc9e8f9d9f8d7c48c8fc87
agraca <anthonygraca@gmail.com>
2017-04-10 14:44:48 -0700

    added driver program

---
+++ divideAndConquer/Matrix/Driver.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+public class Driver
+{
+  public static void main(String[] args)
+  {
+    StrassenMatrixMult s = new StrassenMatrixMult();
+
+    int size = 2;
+    ArrayList<ArrayList<Integer>> a = new Arraylist<ArrayList<Integer>>();
+    ArrayList<ArrayList<Integer>> b = new Arraylist<ArrayList<Integer>>();
+
+    // populate the matrices with random variables
+    for(int i = 0; i < size; i++)
+    {
+      a.add(new ArrayList<Integer>());
+      b.add(new ArrayList<Integer>());
+      for(int j = 0; j < size; j++)
+      {
+        //Generates an integer from 1-100
+        (a.get(i)).add((int)Math.ceil(Math.random()*100));
+        (b.get(i)).add((int)Math.ceil(Math.random()*100));
+      }
+    }
+
+    // Print matrices
+    System.out.println("Matrix a is:");
+    s.printMatrix(a);
+    System.out.println("Matrix b is:");
+    s.printMatrix(b);
+
+    ArrayList<ArrayList<Integer>> c = s.squareMatrixMultiply(a,b);
+    s.printMatrix(c);
+  }
+}
