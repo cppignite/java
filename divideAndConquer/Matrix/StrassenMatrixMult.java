d233a4fae2ccde784ae08ee785ab8314f17f936b
agraca <anthonygraca@gmail.com>
2017-04-10 15:53:02 -0700

    added method signature for splitSubMatrix

--- divideAndConquer/Matrix/StrassenMatrixMult.java
+++ divideAndConquer/Matrix/StrassenMatrixMult.java
@@ -3,7 +3,7 @@ public class StrassenMatrixMult
 {
   /**
   * This method takes in two square nxn matrices and multiplies them together
-  * using Strassen's divide and conquer algorithm. It runs in O(n^2.81)
+  * using a simple divide and conquer method
   *
   * We assume that n is an exact power of 2 so that when we divide an nxn
   * matrix into four n/2 x n/2 matrices, we are guarenteed that n/2 is an
@@ -12,6 +12,19 @@ public class StrassenMatrixMult
   * Idea:
   * The idea is that we partition A, B, and C into four n/2 x n/2 matrices
   * 
+  * A = (a11 a12    B = (b11 b12      C = (c11 c12
+  *      a21 a22),       b21 b22),         c21 c22)
+  * 
+  * Which we can write as,
+  * (c11 c12  = (a11 a12  * (b11 b12
+  *  c21 c22)    a21 a22)    b21 b22)
+  *
+  * Which gives us the 4 equations:
+  * c11 = a11*b11 + a12*b21
+  * c12 = a11*b12 + a12*b22
+  * c21 = a21*b11 + a22*b21
+  * c22 = a21*b12 + a22*b22
+  *
   * @param A - nxn matrix filled with numbers
   * @param B - nxn matrix filled with numbers
   * @return C - nxn matrix that's the product of two input matrices
@@ -23,7 +36,30 @@ public class StrassenMatrixMult
     int n = a.size(); 
     ArrayList<ArrayList<Integer>> c = new ArrayList<ArrayList<Integer>>();
 
+    // base case
+    if (n == 1)
+    {
+      (c.add(ArrayList<Integer>));
+      (a.get(1)).add((a.get(1)).get(1) + (b.get(1)).get(1));
+    }
+    else // partition a,b and c into the 4 equations above w/ indexes so O(1)
+    {
+
+
+    }
+
     return c;
+  }
+  
+  /**
+   * We use this in addition to our recurse matrix multiple method.
+   * We are creating 12 new n/2 x n/2 matrices which would cost O(n^2)
+   **/
+  public ArrayList<ArrayList<Integer>> splitSubMatrix(
+      ArrayList<ArrayList<Integer>> a, int rStart, int rEnd, 
+      int cStart, int cEnd)
+  {
+
   }
 
   /**
