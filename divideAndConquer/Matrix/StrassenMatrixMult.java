86440e0631a300a09d18729c379f945a6979fe42
agraca <anthonygraca@gmail.com>
2017-04-10 14:20:30 -0700

    created strassen mult

---
+++ divideAndConquer/Matrix/StrassenMatrixMult.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class StrassenMatrixMult
+{
+  /**
+   * This method takes in two square nxn matrices and multiplies them together
+   * via divide and conquer.
+   *
+   * @param A - nxn matrix filled with numbers
+   * @param B - nxn matrix filled with numbers
+   * @return C - nxn matrix that's the product of two input matrices
+   **/
+  public ArrayList<ArrayList<Integer>> squareMatrixMultiply(
+      ArrayList<ArrayList<Integer>> a, ArrayList<ArrayList<Integer>> b)
+  {
+    // obtain the dimensions of the square matrix
+    int n = a.size(); 
+    ArrayList<ArrayList<Integer>> c = new ArrayList<ArrayList<Integer>>();
+    return c;
+  }
+}
