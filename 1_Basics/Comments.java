5734d3c17c5a14c87e7f35aa395f1b2c9c9e2135
Byung Hun Lee <byunghunlee@cpp.edu>
2017-12-18 21:23:23 -0800

    Added Comments.java and SimpleMath.java

---
+++ 1_Basics/Comments.java
@@ -0,0 +1,23 @@
+public class Comments
+{
+  public static void main(String[] args)
+  {
+	System.out.println ("Use double forward slash (//) for comments");
+	//This will not show up
+	System.out.println("This will show up");
+	//This will also not show up
+	System.out.println("This will also show up");
+	//What if you have a long comment?
+	//You can keep using double forward slash
+	//But it becomes tedious after a while
+	
+	/*You can have multiple comments
+	by starting with a forward slash and a star
+	and ending with a star and forward slash.
+	just
+	like
+	this!
+	*/
+	
+  }
+}
