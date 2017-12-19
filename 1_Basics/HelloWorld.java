10f93954c5bac0d11772df018210e990898e36e3
agraca <anthonygraca@gmail.com>
2017-12-18 23:23:46 -0800

    adding desc

--- 1_Basics/HelloWorld.java
+++ 1_Basics/HelloWorld.java
@@ -1,3 +1,10 @@
+/******************************************************************************
+ * Description: First Program for Students
+ * Organization: IgniteCS
+ * Date: December 18th, 2017
+ *
+ * @author Anthony Graca
+ *****************************************************************************/
 public class HelloWorld
 {
   public static void main(String[] args)
