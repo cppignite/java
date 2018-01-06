07d501d30ac7bead46d769f8ed1aa01daaaf46ca
Ronald Lencevičius <rlencevicius@gmail.com>
2018-01-05 21:41:07 -0800

    Update BubbleSort.java

--- 4_Arrays/BubbleSort.java
+++ 4_Arrays/BubbleSort.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
@@ -17,7 +17,6 @@ public class BubbleSort {
                     trigger = true;
                 }
             }
-
 
             if (trigger == false)
                 sorted = true;
