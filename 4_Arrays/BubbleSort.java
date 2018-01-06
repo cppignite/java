a24d6cdfb81db538e5e204050e6d35f3317870c3
Ronald Lencevičius <rlencevicius@gmail.com>
2018-01-05 21:35:44 -0800

    Create BubbleSort.java

---
+++ 4_Arrays/BubbleSort.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {3,4,1,2,7,8,5,9,6,0};
+        boolean sorted = false;
+        boolean trigger = false;
+
+        System.out.println(Arrays.toString(arr));
+
+        while (sorted == false) {
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i+1]){
+                    int val = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = val;
+                    trigger = true;
+                }
+            }
+
+
+            if (trigger == false)
+                sorted = true;
+
+            trigger = false;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
